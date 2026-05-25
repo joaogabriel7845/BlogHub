e779d4e7f4a84839d14ead22700e57d517208d3b
Dogcore <aquinojoao373@gmail.com>
2026-05-25 18:41:52 -0300

    feat: adiciona navegação com React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ThemeProvider } from './context/ThemeContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DetailPost from "./pages/DetailPost.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ThemeProvider>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/post/:id" element={<DetailPost />} />
+      </Routes>
+    </ThemeProvider>
+  </BrowserRouter>,
+);
---
+++ src/pages/DetailPost.jsx
@@ -0,0 +1,41 @@
+import { ArrowLeft, StepBack } from "lucide-react";
+import { useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { ThemeContext } from "../context/ThemeContext";
+
+function DetailPost() {
+  const { state } = useLocation();
+  const post = state.post;
+
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <div
+      className={`w-full min-h-screen p-10 flex flex-col gap-5 ${theme.bg} ${theme.text}`}
+    >
+      <Link to={"/"}>
+        <ArrowLeft />
+      </Link>
+      <div
+        className={`w-full flex flex-1 flex-col gap-5 p-10 border ${theme.borderDetailPost} shadow`}
+      >
+        <h1 className="font-[Lora] text-center text-3xl">{post.title}</h1>
+        <div className="flex gap-5">
+          <img
+            className="flex-1 object-cover rounded-md"
+            src={post.img}
+            alt=""
+          />
+          <div
+            className={`flex-1 flex flex-col gap-5 p-5 border ${theme.borderDetailPost}`}
+          >
+            <p className="">Usuário: {post.description}</p>
+            <p className="text-xl font-[Lora]">{post.context}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DetailPost;
