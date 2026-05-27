b7fde727994ffc47727b7f34041766ae46e67412
Dogcore <aquinojoao373@gmail.com>
2026-05-27 08:20:22 -0300

    fix: redireciona para home ao acessar post sem state

--- src/pages/DetailPost.jsx
+++ src/pages/DetailPost.jsx
@@ -1,35 +1,48 @@
 import { ArrowLeft, StepBack } from "lucide-react";
 import { useContext } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, Navigate, useLocation } from "react-router-dom";
 import { ThemeContext } from "../context/ThemeContext";
 
 function DetailPost() {
   const { state } = useLocation();
+
+  if (!state?.post) {
+    return <Navigate to={"/"} />;
+  }
   const post = state.post;
 
   const { theme } = useContext(ThemeContext);
 
   return (
-    <div className={`w-full min-h-screen p-5 md:p-10 flex flex-col gap-5 ${theme.bg} ${theme.text}`}>
-
+    <div
+      className={`w-full min-h-screen p-5 md:p-10 flex flex-col gap-5 ${theme.bg} ${theme.text}`}
+    >
       <Link className="w-fit" to={"/"}>
         <ArrowLeft />
       </Link>
 
-      <div className={`w-full flex flex-1 flex-col gap-5 md:p-10 border-0 md:border ${theme.borderDetailPost}`}>
-
+      <div
+        className={`w-full flex flex-1 flex-col gap-5 md:p-10 border-0 md:border ${theme.borderDetailPost}`}
+      >
         <h1 className="font-[Lora] text-center text-3xl">{post.title}</h1>
 
-        <div className={`flex flex-1 flex-wrap gap-5 items-center md:border ${theme.borderDetailPost} md:p-5`}>
+        <div
+          className={`flex flex-1 flex-wrap gap-5 items-center md:border ${theme.borderDetailPost} md:p-5`}
+        >
+          <img
+            className="flex-1 object-cover rounded-md"
+            src={post.img}
+            alt=""
+          />
 
-          <img className="flex-1 object-cover rounded-md" src={post.img} alt=""/>
-
-          <div className={`flex-1 flex flex-col gap-5 p-5 border justify-between ${theme.borderDetailPost}`}>
-
-            <p className="">{post.user}: {post.description}</p>
+          <div
+            className={`flex-1 flex flex-col gap-5 p-5 border justify-between ${theme.borderDetailPost}`}
+          >
+            <p className="">
+              {post.user}: {post.description}
+            </p>
 
             <div className={`flex flex-col divide-y ${theme.divider}`}>
-
               <div className="py-4">
                 <p className="text-xl font-[Lora]">{post.context}</p>
               </div>
@@ -37,16 +50,13 @@ function DetailPost() {
               <div className="py-4">
                 <p className="text-xl font-[Lora]">{post.context2}</p>
               </div>
-
             </div>
 
             <div className="flex justify-between">
-                <p>{post.date}</p>
-                <p>por {post.user}</p>
+              <p>{post.date}</p>
+              <p>por {post.user}</p>
             </div>
-
           </div>
-
         </div>
       </div>
     </div>
